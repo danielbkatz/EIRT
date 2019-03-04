eb42ead647c2e442c77152be953e1691d8e986cf
Daniel Katz <katzdanb@gmail.com>
2019-03-03 19:11:56 -0800

    EIRT data for Tutorial

--- reading in the data.R
+++ reading in the data.R
@@ -1,7 +1,8 @@
 install.packages("RCurl")
 library(RCurl)
-data1 <- getURL("https://raw.githubusercontent.com/danielbkatz/sum_explanatory_work/EIRT_tutorial/EIRTdata.csv")
+data1 <- getURL("https://raw.githubusercontent.com/danielbkatz/EIRT/master/eirtdata.csv")
 eirtdata <- read.csv(text = data1)
-names(eirtdata)[1] <- "id"
+eirtdata <- eirtdata[-1]
 
+#optional, if you want to save the data as a csv file on your computer. Note, it adds a column.
 write.csv(eirtdata, "eirtdata.csv")
